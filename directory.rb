f2a91b869db5d56bca43dca781838ca47a89794a
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-22 15:04:09 +0100

    Adds a list and a total of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we print a list of students, each one on a new line
 puts "The students of Villains Academy"
 puts "-----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Total number of students - print doesn't add new lines
+print "Overall, we have"
+print 11
+puts " great students"
