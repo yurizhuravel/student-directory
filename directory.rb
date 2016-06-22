232aaa21c3391385b5bd40d3d5028248a8d66b0b
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-22 16:51:23 +0100

    Adds numbers to student names

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ def print_header
 end
 
 def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  students.each.with_index(1) { |student, index| puts " #{index}: #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer(names)
