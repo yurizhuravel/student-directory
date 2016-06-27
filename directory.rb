b75109471459ad10d375ac8ec4e4f7a2f8f6f0a9
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-27 14:58:01 +0100

    Adds interactive menu

--- directory.rb
+++ directory.rb
@@ -17,27 +17,22 @@ def input_students
   #get the age
   take_input("age")
   age = gets.chomp
-  #get the height
-  take_input("height")
-  height = gets.chomp
 
   #while the name is not empty, repeat this:
   while !name.empty?
-    students << {name: name, cohort: cohort, age: age, height: height}
+    students << {name: name, cohort: cohort, age: age}
     if students.length == 1
       puts "Now we have #{students.count} student"
     else
       puts "Now we have #{students.count} students"
     end
-    #get more names
+    #get more data
     take_input("name")
     name = gets.chomp
     take_input("cohort")
     cohort = gets.chomp
     take_input("age")
     age = gets.chomp
-    take_input("height")
-    height = gets.chomp
   end
 
   #return the array of students
@@ -45,7 +40,7 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(50)
+  puts "The students of Villains Academy by cohort:".center(50)
   puts "-----------------".center(50)
 end
 
@@ -61,8 +56,31 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(50)
 end
 
-#Then we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    # print the menu and ask user what to do
+    puts "1. Input the students list"
+    puts "2. Show the students list"
+    puts "9. Quit" # 9 because we'll be adding more items
+    # 2. save the input into a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+      when "1"
+        # input the students list
+        students = input_students
+      when "2"
+        # show the students list
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I didin't get that, try again"
+    end
+  end
+end
+
+interactive_menu
